0dd09d66acea67d757249c812db08548720dc6a4
Steven Turturo <steven@avocagrow.com>
2023-04-19 15:21:11 -0500

    added github workflows

--- infra/main.go
+++ infra/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/container"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	Pulumi.Run(func(ctx *pulumi.Context) error {
+		registry, err := container.NewRegistry(ctx, "registry", &container.RegistryArgs{
+            Project: pulumi.String("avocagrow-internal-tools"),
+            Location: pulumi.String("")
+        })
+		if err != nil {
+			return err
+		}
 		return nil
 	})
 }
