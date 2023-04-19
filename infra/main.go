6a4a7a925d8af4b98c80f3f95f755611d15d51e9
Steven Turturo <steven@avocagrow.com>
2023-04-19 16:25:53 -0500

    added basic code to run in App engine

--- infra/main.go
+++ infra/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/container"
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/appengine"
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/projects"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := container.NewRegistry(ctx, "registry", &container.RegistryArgs{
-			Project:  pulumi.String("avocagrow-internal-tools"),
-			Location: pulumi.String("US"),
+		project, err := projects.GetProject(ctx, &projects.GetProjectArgs{
+			Filter: "name:avocagrow-internal-tools",
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = appengine.NewApplication(ctx, "doppler-example", &appengine.ApplicationArgs{
+			Project:    pulumi.String(project.Id),
+			LocationId: pulumi.String("us-central"),
 		})
 		if err != nil {
 			return err
