e7a058f73e4b4e95c99782345450b34c45034cfe
Steven Turturo <steven@avocagrow.com>
2023-05-02 06:11:13 -0500

    testing some more pulumi code

--- infra/main.go
+++ infra/main.go
@@ -2,28 +2,19 @@ package main
 
 import (
 	"github.com/pulumi/pulumi-gcp/sdk/v5/go/gcp/storage"
-	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/appengine"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-        _, err := appengine.NewApplication(ctx, "doppler-example", &appengine.ApplicationArgs{
-			Project:    pulumi.String("avocagrow-internal-tools"),
-			LocationId: pulumi.String("us-central"),
+        _, err := storage.NewBucket(ctx, "doppler-example", &storage.BucketArgs{
+			Location: pulumi.String("US"),
 		})
 		if err != nil {
 			return err
 		}
 
-        _, err = storage.NewBucket(ctx, "doppler-example", &storage.BucketArgs{
-            Location: pulumi.String("US"),
-        })
-		if err != nil {
-			return err
-		}
-
-        // Set up load balancer for serverless application (i.e. appengine)
+		// Set up load balancer for serverless application (i.e. appengine)
 
 		return nil
 	})
